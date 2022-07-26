0f238fec230f7160429b2ad2d5bcb6356291c750
houssemnabouli@gmail.com <21754318h>
2022-07-26 15:35:51 +0100

    First Sprint completed

--- WebApi/Controllers/Users/ClientController.cs
+++ WebApi/Controllers/Users/ClientController.cs
@@ -2,11 +2,15 @@
 using Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers.Clients
@@ -16,11 +20,17 @@ namespace WebApi.Controllers.Clients
     public class ClientController : ControllerBase
     {
         private readonly IClientService ClientService;
+        private readonly UserManager<Utilisateur> _userManager;
+        private readonly IConfiguration configuration;
+        private readonly IMailingService mailingService;
 
-        public ClientController(IClientService _ClientService)
+        public ClientController(IClientService _ClientService, UserManager<Utilisateur> userManager, IConfiguration configuration, IMailingService mailingService)
         {
 
             ClientService = _ClientService;
+           _userManager = userManager;
+            this.configuration = configuration;
+            this.mailingService = mailingService;
         }
         [AllowAnonymous]
         [HttpPost("Register")]
@@ -34,7 +44,22 @@ namespace WebApi.Controllers.Clients
 
             if (!result.IsAuthenticated)
                 return BadRequest(result.Message);
+            //Confirmations mail
+            var user = await _userManager.FindByEmailAsync(result.Email);
+            var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);
+            var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);
+            string url = $"https://localhost:44353/api/User/confirmemail?userid={user.Id}&token={validEmailToken}";
+            var filePath = $"{Directory.GetCurrentDirectory()}\\Templates\\EmailTemplate.html";
+            var str = new StreamReader(filePath);
 
+            var mailText = str.ReadToEnd();
+            str.Close();
+
+            mailText = mailText.Replace("[username]", user.UserName).Replace("[email]", user.Email).Replace("[URL]", url);
+
+            await mailingService.SendEmailAsync(user.Email, "Welcome to BigSoft" +
+                "", mailText);
             return Ok(result);
         }
         [HttpPost("token")]
@@ -107,6 +132,31 @@ namespace WebApi.Controllers.Clients
 
             return Entity;
 
+        }
+        [AllowAnonymous]
+        [HttpGet("ForgetPassword")]
+        public async Task<UserManagerResponseModel> ForgetPasswordAsync(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                return new UserManagerResponseModel
+                {
+                    IsSuccess = false,
+                    Message = "No user associated with email",
+                };
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var encodedToken = Encoding.UTF8.GetBytes(token);
+            var validToken = WebEncoders.Base64UrlEncode(encodedToken);
+
+
+
+            return new UserManagerResponseModel
+            {
+                IsSuccess = true,
+                Message = "Reset password URL has been sent to the email successfully!",
+                token = validToken
+            };
         }
     }
 }
--- WebApi/Controllers/Users/FournisseurController.cs
+++ WebApi/Controllers/Users/FournisseurController.cs
@@ -2,11 +2,16 @@
 using Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers.Users
@@ -16,10 +21,16 @@ namespace WebApi.Controllers.Users
     public class FournisseurController : ControllerBase
     {
         private readonly IFournisseurService fournisseurService;
+        private readonly UserManager<Utilisateur> _userManager;
+        private readonly IConfiguration configuration;
+        private readonly IMailingService mailingService;
 
-        public FournisseurController(IFournisseurService _FournisseurService)
+        public FournisseurController(IFournisseurService _FournisseurService, UserManager<Utilisateur> userManager, IConfiguration configuration, IMailingService mailingService)
         {
             fournisseurService = _FournisseurService;
+           _userManager = userManager;
+            this.configuration = configuration;
+            this.mailingService = mailingService;
         }
         [AllowAnonymous]
         [HttpPost("Register")]
@@ -33,7 +44,21 @@ namespace WebApi.Controllers.Users
 
             if (!result.IsAuthenticated)
                 return BadRequest(result.Message);
+            var user = await _userManager.FindByEmailAsync(result.Email);
+            var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);
+            var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);
+            string url = $"https://localhost:44353/api/User/confirmemail?userid={user.Id}&token={validEmailToken}";
+            var filePath = $"{Directory.GetCurrentDirectory()}\\Templates\\EmailTemplate.html";
+            var str = new StreamReader(filePath);
 
+            var mailText = str.ReadToEnd();
+            str.Close();
+
+            mailText = mailText.Replace("[username]", user.UserName).Replace("[email]", user.Email).Replace("[URL]", url);
+
+            await mailingService.SendEmailAsync(user.Email, "Welcome to BigSoft" +
+                "", mailText);
             return Ok(result);
         }
         [HttpPost("token")]
@@ -106,6 +131,31 @@ namespace WebApi.Controllers.Users
 
             return Entity;
 
+        }
+        [AllowAnonymous]
+        [HttpGet("ForgetPassword")]
+        public async Task<UserManagerResponseModel> ForgetPasswordAsync(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                return new UserManagerResponseModel
+                {
+                    IsSuccess = false,
+                    Message = "No user associated with email",
+                };
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var encodedToken = Encoding.UTF8.GetBytes(token);
+            var validToken = WebEncoders.Base64UrlEncode(encodedToken);
+
+
+
+            return new UserManagerResponseModel
+            {
+                IsSuccess = true,
+                Message = "Reset password URL has been sent to the email successfully!",
+                token = validToken
+            };
         }
     }
 }
